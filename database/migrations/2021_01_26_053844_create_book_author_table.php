a8ab4c74d0259a06485a261a627eee000495837a
Semailk <sem.alik@mail.ru>
2021-01-26 12:46:47 +0400

    a table that links authors and books

---
+++ database/migrations/2021_01_26_053844_create_book_author_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBookAuthorTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('book_author', function (Blueprint $table) {
+            $table->integer('book_id');
+            $table->integer('author_id');
+            $table->timestamps();
+
+            $table->foreign('book_id')
+                ->on('books')
+                ->references('id')
+                ->onDelete('cascade');
+
+            $table->foreign('author_id')
+                ->on('authors')
+                ->references('id')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('book_author');
+    }
+}
